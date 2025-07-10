7030578f2d80ab52c8e04e3a0a2e2039cf694fd0
obutr <butrym@karazin.ua>
2025-07-10 20:43:27 +0300

    Add unit test for battle logic

--- src/test/kotlin/BattleTest.kt
+++ src/test/kotlin/BattleTest.kt
@@ -1,8 +1,20 @@
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
+import softserve.academy.Army
+import softserve.academy.Warrior
+import softserve.academy.addUnits
+import softserve.academy.fight
 
 class BattleTest {
     @Test
-    fun name() {
-        TODO("Not yet implemented")
+    fun `1 Battle`() {
+        val army1 = Army()
+        army1.addUnits(1) { Warrior() }
+        val army2 = Army()
+        army2.addUnits(2) { Warrior() }
+
+        val res = fight(army1, army2)
+
+        assertFalse(res) { "Army of 1 Warrior should loose to army of 2 Warriors" }
     }
 }
